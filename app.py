1a6c00d14612d9d780236d90328e55c6b164c3e3
majima0 <ht19a078@oecu.jp>
2026-08-15 19:10:24 +0900

    test11

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import subprocess
 from flask import Flask, render_template
 
@@ -6,8 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # git diff を取得
+    if os.path.exists("git-search"):
+        shutil.rmtree("git-search")
+
     result = subprocess.run(
-        ["rm -rf git-search || git clone https://github.com/yuuto-afk/git-search.git && cd git-search && git diff HEAD~1 HEAD"],  # 例: 1つ前のコミットとの差分
+        ["git clone https://github.com/yuuto-afk/git-search.git && cd git-search && git diff HEAD~1 HEAD"],  # 例: 1つ前のコミットとの差分
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
