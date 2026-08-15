b981d48b26f98b990a17ebcec69e0f7c6c8bd7bf
majima0 <ht19a078@oecu.jp>
2026-08-15 18:11:09 +0900

    test4

--- app.py
+++ app.py
@@ -1,35 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <title>vue_pager</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html.min.css">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jsdiff/3.4.0/diff.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/diff2html/2.3.3/diff2html-ui.min.js"></script>
-    <script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const unifiedDiff = JsDiff.createPatch("fileName", "a\nb\nc\d\ne\n\nf", "a\nb\nac\d\ne\n\n\nf", "test", "test");
-        const diff2htmlUi = new Diff2HtmlUI({diff: unifiedDiff});
-        diff2htmlUi.draw('#app', {inputFormat: 'json', showFiles: true, matching: 'lines'});
-    });
-    </script>
-    </head>
-    <body>
-    <div id="app">
-    </div>
-    </body>
-    </html>
-    '''
+@app.route("/")
+def index():
+    old = 'console.log("Hello");'
+    new = 'console.log("Hello World");'
+    return render_template("diff.html", old=old, new=new)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run()
