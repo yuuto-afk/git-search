22de01eda661c8d8de5484f9963dab80bc0c02b0
majima0 <ht19a078@oecu.jp>
2026-08-16 14:12:27 +0900

    test12

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import os
 import shutil
 import subprocess
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("diff.html")
+
+@app.route("/diff", methods=["POST"])
+def diff():
+    data = request.get_json()
+    repo_url = data["url"]
+
     # git diff を取得
-    if os.path.exists("git-search"):
-        shutil.rmtree("git-search")
+    if os.path.exists("repo-dir"):
+        shutil.rmtree("repo-dir")
 
     result = subprocess.run(
-        ["git clone https://github.com/yuuto-afk/git-search.git && cd git-search && git diff HEAD~1 HEAD"],  # 例: 1つ前のコミットとの差分
+        ["git clone https://github.com/yuuto-afk/git-search.git repo-dir && cd repo-dir && git diff HEAD~1 HEAD"],  # 例: 1つ前のコミットとの差分
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -23,7 +30,5 @@ def index():
     print(diff_text)
     print(result.stderr)
 
-    return render_template("diff.html", diff=diff_text)
+    return jsonify({"diff": diff_text})
 
-if __name__ == "__main__":
-    app.run()
